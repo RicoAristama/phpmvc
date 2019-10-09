e7cebda5c66fa219c644f6c4688e49044e6ad7d6
Rico Aristama <55724731+RicoAristama@users.noreply.github.com>
2019-10-09 17:22:34 +0700

    Upload phpmvc

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<h1 class="mt-3">About Me</h1>
+	<img src="<?= BASEURL ?>/img/profile.jpg" alt="Rico Aristama" width="300" class="rounded-circle shadow">
+	<p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
